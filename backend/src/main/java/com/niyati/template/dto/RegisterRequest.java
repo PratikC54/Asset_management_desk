c6e3032478256eb7ad9399f7675e25986e4e99b7
PratikC54 <pratiksenapati54@gmail.com>
2026-07-30 22:54:21 +0530

    creating Register DTO

---
+++ backend/src/main/java/com/niyati/template/dto/RegisterRequest.java
@@ -0,0 +1,25 @@
+package com.niyati.template.dto;
+
+import com.niyati.template.models.DEPARTMENT;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String name;
+
+    @Email(message = "Entered mail-id is not valid one")
+    private String email;
+
+    @Size(min = 6, message = "Password least have 6 characters")
+    private String password;
+
+    private DEPARTMENT department;
+
+}
