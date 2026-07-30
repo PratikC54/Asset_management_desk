5bf2dbedee04d4590a6950447c61403d508c1f1f
PratikC54 <pratiksenapati54@gmail.com>
2026-07-30 22:53:17 +0530

    creating user repository layer

---
+++ backend/src/main/java/com/niyati/template/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.niyati.template.repository;
+
+import com.niyati.template.models.USER_ROLE;
+import com.niyati.template.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    User findByRole(USER_ROLE userRole);
+
+    List<User> findAllByRole(USER_ROLE userRole);
+}
