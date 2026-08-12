5a35ca90a4d2cbdabd8f5ce7fd4a6c066391a91d
PratikC54 <pratiksenapati54@gmail.com>
2026-08-12 23:29:04 +0530

    LoginRequest DTO

--- backend/src/main/java/com/niyati/template/dto/LoginRequest.java
+++ backend/src/main/java/com/niyati/template/dto/LoginRequest.java
@@ -1,9 +1,15 @@
 package com.niyati.template.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
+    @Email
+    @NotEmpty
     private String email;
+
+    @NotEmpty
     private String password;
 }
