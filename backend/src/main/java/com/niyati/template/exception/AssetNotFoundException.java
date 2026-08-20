91a5fccf06e1929943c3802c8ef00641126c7ed3
PratikC54 <pratiksenapati54@gmail.com>
2026-08-20 23:22:05 +0530

    creating custom AssetNotFoundException

---
+++ backend/src/main/java/com/niyati/template/exception/AssetNotFoundException.java
@@ -0,0 +1,7 @@
+package com.niyati.template.exception;
+
+public class AssetNotFoundException extends RuntimeException{
+    public AssetNotFoundException(String message) {
+        super(message);
+    }
+}
